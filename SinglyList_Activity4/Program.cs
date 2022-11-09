cc172ec2d369fb4b42311ffc11ceaac023b30e25
TA1128 <m.khaidar.ft21@mail.umy.ac.id>
2022-11-09 17:18:05 +0700

    mengkondisikan while

--- SinglyList_Activity4/Program.cs
+++ SinglyList_Activity4/Program.cs
@@ -7,5 +7,51 @@
         public node next;
     }
 
-    class node
+    class List
+    {
+        node START;
+        public List()
+        {
+            START = null;
+        }
+
+        public void addNode()
+        {
+            int nim;
+            string nm;
+            Console.Write("\nMasukkan nomor Mahasiswa: ");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nMasukkan nama Mahasiswa");
+            nm = Console.ReadLine();
+            node nodeBaru = new node();
+            nodeBaru.noMhs = nim;
+            nodeBaru.nama = nm;
+
+            if (START == null || nim <= START.noMhs)
+            {
+                if((START != null) && (nim == START.noMhs))
+                {
+                    Console.WriteLine("\nNomor mahasiswa sama tidak diijinkan\n");
+                    return;
+                }
+                node Previous, current;
+                Previous = START;
+                current = START;
+
+                while ((current != null) && (nim >= current.noMhs))
+                {
+                    if (nim == current.noMhs)
+                    {
+                        Console.WriteLine("\nNomor Mahasiswa sama tidak diijinkan\n");
+                        return;
+                    }
+                    Previous = current;
+                    current = current.next;
+                }
+
+                nodeBaru.next = current;
+                Previous.next = nodeBaru;
+            }
+        }
+    }
 }
